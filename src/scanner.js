ac0a53645afd5d60c9122c889ce286d98ca39f2a
amine <elkhabli@icgnet.it>
2024-11-25 11:59:20 +0100

    modifica installa dei package tgz

--- src/scanner.js
+++ src/scanner.js
@@ -6,7 +6,7 @@ const child_process = require("child_process");
 const async = require("async");
 const rimraf = require("rimraf");
 const colors = require('colors/safe');
-const svnUltimate = require('../scripts/svn-command');
+const svnCommand = require('../scripts/svn-command');
 
 const cacheFile = __dirname + "/../.cache";
 const rootDir = path.resolve(__dirname, "../..")
@@ -32,13 +32,11 @@ async.each(svnDependencyObject,
 	function (svnDependency, callback) {
 		async.series(
 			[
-				// validateCache(svnDependency),
 				mkdirs(svnDependency),
 				checkout(svnDependency),
 				cleanup(svnDependency),
 				update(svnDependency),
 				cleanup(svnDependency),
-				writeToCache(svnDependency),
 				npmInstall(svnDependency)
 			],
 			info(svnDependency, callback)
@@ -48,74 +46,6 @@ async.each(svnDependencyObject,
     	writeBufferToCache();
 	}
 );
-
-function buildDepObj(svnDependency, packageJsonDeps) {
-    let out = {};
-    out.repo = packageJsonDeps[svnDependency];
-
-	// --- verifico se c'è la revision
-	if (svnDependency.indexOf("|") > 0) {
-        svnDependency = /^(.*)\|(.*)$/.exec(svnDependency);
-        out.name = svnDependency[1];
-        out.rev = svnDependency[2];
-
-	// --- se nel link trovo il trunk o altro imposto la revision come HEAD
-    } else {
-        out.name = svnDependency;
-        out.rev = "HEAD";
-    }
-    out.COPath = out.repo;
-
-    out.installDir = "_icg-packages/"+out.name;
-
-	return out;
-}
-
-function writeToCache(svnDependency) {
-    return function (callback) {
-        CACHEBUFFER.push(svnDependency);
-        callback(null);
-    }
-}
-
-function writeBufferToCache() {
-    const cacheJson = _readCache();
-    CACHEBUFFER.forEach(
-		function (dep) {
-        	cacheJson[dep.name] = dep;
-    	}
-	);
-
-	return writeCache(cacheJson)( function () {} );
-}
-
-function writeCache(data) {
-    return function (callback) {
-        fs.writeFile(
-			cacheFile,
-			JSON.stringify(data),
-			function (error) {
-            	callback(error)
-        	}
-		)
-    }
-}
-
-function _readCache() {
-    return fs.existsSync(cacheFile) ? JSON.parse(fs.readFileSync(cacheFile, "utf8")) : {};
-}
-
-function validateCache(dep) {
-    return function (callback) {
-        const cacheJson = _readCache();
-        const svnDependencyCached = cacheJson[dep.name];
-        if (svnDependencyCached) {
-            dep.latest = svnDependencyCached.rev === dep.rev;
-        }
-
-        return callback(null);
-    }
-}
 
 function mkdirs(svnDependency) {
     return function (callback) {
@@ -188,6 +118,29 @@ function mkdirs(svnDependency) {
     };
 }
 
+function buildDepObj(svnDependency, packageJsonDeps) {
+    let out = {};
+    out.repo = packageJsonDeps[svnDependency];
+
+	// --- verifico se c'è la revision
+	if (svnDependency.indexOf("|") > 0) {
+        svnDependency = /^(.*)\|(.*)$/.exec(svnDependency);
+        out.name = svnDependency[1];
+        out.rev = svnDependency[2];
+
+	// --- se nel link trovo il trunk o altro imposto la revision come HEAD
+    } else {
+        out.name = svnDependency;
+        out.rev = "HEAD";
+    }
+    out.COPath = out.repo;
+
+    out.installDir = "."+out.name+"-package";
+    // out.installDir = "../."+out.name+"-package";
+
+	return out;
+}
+
 function checkout(svnDependency) {
 	return function (callback) {
 		console.log(
@@ -201,33 +154,53 @@ function checkout(svnDependency) {
 			callback(null);
 		} else {
 
-			const svnUltimateUrl = svnDependency.COPath
-			const svnUltimatePath = path.resolve(rootDir, svnDependency.installDir)
-			const svnUltimateOption = {
-				trustServerCert: true,
-				username: svnOptions.username,
-				password: svnOptions.password,
-				quiet: true,
-				force: true,
-				revision: svnDependency.rev,
-				ignoreExternals: false,
-				depth: '', // Valore valido
-			};
+			const svnUrl = svnDependency.COPath
+			const svnPath = path.resolve(rootDir, svnDependency.installDir)
+			const svnOption = Object.assign({ revision: svnDependency.rev }, svnOptions)
 
-			svnUltimate.commands.checkout(
-				svnUltimateUrl,
-				svnUltimatePath,
-				svnUltimateOption,
-				function( error ) {
+			// --- svn interface
+			svnCommand.checkout(
+				svnUrl,
+				svnPath,
+				svnOption,
+				function (error, result) {
 					console.log(
 						colors.green("Checkout finished!"),
 						colors.yellow(svnDependency.name),
 						"rev=" + svnDependency.rev,
-						"from", svnDependency.COPath
+						"from", svnDependency.COPath,
+						"installDirPath=", svnPath
 					);
-					return callback(error || null)
+					return callback(error ? result : null)
 				}
 			)
+
+			// --- svn ultimate
+			// const svnOption = {
+			// 	trustServerCert: true,
+			// 	username: svnOptions.username,
+			// 	password: svnOptions.password,
+			// 	quiet: true,
+			// 	force: true,
+			// 	revision: svnDependency.rev,
+			// 	ignoreExternals: false,
+			// 	depth: '', // Valore valido
+			// };
+
+			// svnCommand.commands.checkout(
+			// 	svnUrl,
+			// 	svnPath,
+			// 	svnOption,
+			// 	function( error ) {
+			// 		console.log(
+			// 			colors.green("Checkout finished!"),
+			// 			colors.yellow(svnDependency.name),
+			// 			"rev=" + svnDependency.rev,
+			// 			"from", svnDependency.COPath
+			// 		);
+			// 		return callback(error || null)
+			// 	}
+			// )
 		}
     }
 }
@@ -235,59 +208,100 @@ function checkout(svnDependency) {
 function update(svnDependency) {
 	return function (callback) {
 
+		const svnPath = path.normalize(path.join(rootDir, svnDependency.installDir))
+		const svnOption = Object.assign({ revision: svnDependency.rev }, svnOptions)
+
 		console.log(
 			colors.magenta("START Update "),
 			colors.yellow(svnDependency.name),
 			"rev=" + svnDependency.rev,
-			"installDir ", svnDependency.installDir
+			"installDirPath=", svnPath
 		)
 
-		const svnUltimatePath = path.normalize(path.join(rootDir, svnDependency.installDir))
-		const svnUltimateOption = {
-			trustServerCert: true,	// same as --trust-server-cert
-			username: svnOptions.username,	// same as --username
-			password: svnOptions.password,	// same as --password
-			quiet: true,			// provide --quiet to commands that accept it
-			force: true,			// provide --force to commands that accept it
-			revision: svnDependency.rev,		// provide --revision to commands that accept it,
-			ignoreExternals: false,
-			depth: ''
-		}
-		return svnUltimate.commands.update(
-			svnUltimatePath,
-			svnUltimateOption,
-			function (error) {
+		// --- svn interface
+		svnCommand.update(
+			svnPath,
+            svnOption,
+            function (error, result) {
 				console.log(
 					colors.magenta("Update finished!"),
 					colors.yellow(svnDependency.name),
 					"rev=" + svnDependency.rev,
-					"installDir ", svnDependency.installDir
+					"installDirPath=", svnPath
 				)
-
-				return callback(error || null)
-			}
+				return callback(error ? result : null)
+        	}
 		)
+		// --- svn ultimate
+		// const svnOption = {
+		// 	trustServerCert: true,	// same as --trust-server-cert
+		// 	username: svnOptions.username,	// same as --username
+		// 	password: svnOptions.password,	// same as --password
+		// 	quiet: true,			// provide --quiet to commands that accept it
+		// 	force: true,			// provide --force to commands that accept it
+		// 	revision: svnDependency.rev,		// provide --revision to commands that accept it,
+		// 	ignoreExternals: false,
+		// 	depth: ''
+		// }
+		// return svnCommand.commands.update(
+		// 	svnPath,
+		// 	svnOption,
+		// 	function (error) {
+		// 		console.log(
+		// 			colors.magenta("Update finished!"),
+		// 			colors.yellow(svnDependency.name),
+		// 			"rev=" + svnDependency.rev,
+		// 			"installDirPath=", svnPath
+		// 		)
+
+		// 		return callback(error || null)
+		// 	}
+		// )
     }
 }
 
 function cleanup(svnDependency) {
     return function (callback) {
-		console.log(colors.cyan("START Cleanup "));
+		const svnPath = path.normalize(path.join(rootDir, svnDependency.installDir))
 
-		const svnUltimatePath = path.normalize(path.join(rootDir, svnDependency.installDir))
-		const svnUltimateOption = {
-			username: svnOptions.username,	// same as --username
-			password: svnOptions.password,	// same as --password
-		}
-		return svnUltimate.commands.cleanup(
-			svnUltimatePath,
-			svnUltimateOption,
-			function (error) {
-				console.log(colors.cyan("Cleanup finished!"));
+		console.log(
+			colors.cyan("START Cleanup "),
+			colors.yellow(svnDependency.name),
+			"installDirPath=", svnPath
+		);
 
-				return callback(error || null)
-			}
+		// --- svn interface
+		svnCommand.cleanup(
+			svnPath,
+			svnOptions,
+			function (error, result) {
+				console.log(
+					colors.cyan("Cleanup finished!"),
+					colors.yellow(svnDependency.name),
+					"installDirPath=", svnPath
+				);
+				return callback(error ? result : null)
+        	}
 		)
+
+		// --- svn ultimate
+		// const svnOption = {
+		// 	username: svnOptions.username,	// same as --username
+		// 	password: svnOptions.password,	// same as --password
+		// }
+		// return svnCommand.commands.cleanup(
+		// 	svnPath,
+		// 	svnOption,
+		// 	function (error) {
+				// console.log(
+				// 	colors.cyan("Cleanup finished!"),
+				// 	colors.yellow(svnDependency.name),
+				// 	"installDirPath=", svnPath
+				// );
+
+		// 		return callback(error || null)
+		// 	}
+		// )
     }
 }
 
@@ -297,7 +311,7 @@ function npmInstall(svnDependency) {
 		const eKeys = Object.keys(process.env), env = {};
 		let i;
 
-		console.log(colors.yellow("Running `npm install` on "), svnDependency.name, "...");
+		console.log(colors.bgWhite.black("Running `npm install` on "), svnDependency.name, "...");
 
 
         for (i = eKeys.length; i--;) {
@@ -308,7 +322,7 @@ function npmInstall(svnDependency) {
 
 		const directoryPath = path.normalize(path.join(rootDir, svnDependency.installDir))
 
-		console.log(colors.gray("START Install " + directoryPath));
+		console.log(colors.bgWhite.blue("START Install " + directoryPath));
 
 		let fileTgz = ""
 		const files = fs.readdirSync(directoryPath);
@@ -322,20 +336,29 @@ function npmInstall(svnDependency) {
 			}
 		);
 
+		console.log(colors.bgWhite.blue("Install command = npm install --no-save --no-package-lock --legacy-peer-deps " + fileTgz));
+
 		child_process.exec(
-			"npm install --production --force " + fileTgz,
+			"npm install --no-save --no-package-lock --legacy-peer-deps " + fileTgz,
 			{
 				stdio: "inherit",
 				cwd: appDir,
 				env: env
 			},
 			function (error) {
-				console.log(colors.gray("Install finished, " + error));
-		    	callback(error ? "npm install failed" : null);
+				console.log(colors.bgWhite.blue("Install finished, " + error));
+				const pathModule = path.resolve(rootDir, svnDependency.installDir)
+				rimraf(
+					pathModule,
+					function (error) {
+						callback(error ? "npm install failed" : null);
+					}
+				);
 			}
 		);
     };
 }
+
 function info(svnDependency, callback) {
     return function (error) {
         if (error) {
@@ -362,4 +385,50 @@ function info(svnDependency, callback) {
         }
         callback();
     };
+}
+
+function writeToCache(svnDependency) {
+    return function (callback) {
+        CACHEBUFFER.push(svnDependency);
+        callback(null);
+    }
+}
+
+function writeBufferToCache() {
+    const cacheJson = _readCache();
+    CACHEBUFFER.forEach(
+		function (dep) {
+        	cacheJson[dep.name] = dep;
+    	}
+	);
+
+	return writeCache(cacheJson)( function () {} );
+}
+
+function writeCache(data) {
+    return function (callback) {
+        fs.writeFile(
+			cacheFile,
+			JSON.stringify(data),
+			function (error) {
+            	callback(error)
+        	}
+		)
+    }
+}
+
+function _readCache() {
+    return fs.existsSync(cacheFile) ? JSON.parse(fs.readFileSync(cacheFile, "utf8")) : {};
+}
+
+function validateCache(dep) {
+    return function (callback) {
+        const cacheJson = _readCache();
+        const svnDependencyCached = cacheJson[dep.name];
+        if (svnDependencyCached) {
+            dep.latest = svnDependencyCached.rev === dep.rev;
+        }
+
+        return callback(null);
+    }
 }
